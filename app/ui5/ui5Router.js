a1a342f32ddea0761e6bb52163a52c6dff0922f9
Dagmito <36495952+dagmito@users.noreply.github.com>
2018-04-26 23:16:46 -0300

    Commit feito depois da rota do ui5
     Changes to be committed:
    	new file:   app/ui5/ui5Router.js
    	modified:   config/config.js
    	modified:   package-lock.json
    	modified:   package.json

---
+++ app/ui5/ui5Router.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    getRouter: (express)=>{ // arrow funtion
+        var path = require('path');
+        return express.Router()
+        .use('/', express.static(path.join(__dirname,'webapp')));
+
+    }
+
+}
--- config/config.js
+++ config/config.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
+var consign = require('consign');
+
+var ui5 = require('../app/ui5/ui5Router');
+
+app.use('/', ui5Router.getRouter(express));
 
 module.exports = function(){
     return app;
